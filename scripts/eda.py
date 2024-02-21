d41fc53c34e2bcbdf890e12f649e346092af93f2
Sara-Iftikhar <sara.rwpk@gmail.com>
2024-02-21 11:30:10 +0300

    edited requirements.txt

--- scripts/eda.py
+++ scripts/eda.py
@@ -46,38 +46,38 @@ feature_names = ['TTTTTTGCTAGCGGAAAACGGAGATTTAAAAGAAAACAAAATATTTTTTGCGTA',
 # converting sparse matrix to a numpy array
 X_array = X.toarray()
 
-# # %%
-#
-# # plotting heatmap
-#
-# whole_data = np.concatenate((X_array, y.values.reshape(-1, 1)), axis=1)
-# _ = associations(
-#     whole_data,
-#     nom_nom_assoc="cramer",
-#     fmt=".1f",
-#     figsize=(8, 8),
-#     plot=False,
-#     cbar=True
-# )
-# plt.tight_layout()
-# plt.show()
-#
-# # %%
-#
-# # plotting input features
-#
-# fig, axes = create_subplots(10, figsize=(14,10))
-#
-# for col, ax in zip(range(X.shape[1]), axes.flatten()):
-#
-#     pie(X_array[:, col], ax=ax,
-#         ax_kws=dict(title=f'input feature {col+1}'), show=False)
-#
-# plt.tight_layout()
-# plt.show()
-#
-# # %%
-#
-# # pie chart of target
-#
-# pie(y, ax_kws=dict(title='Target'))
+# %%
+
+plotting heatmap
+
+whole_data = np.concatenate((X_array, y.values.reshape(-1, 1)), axis=1)
+_ = associations(
+    whole_data,
+    nom_nom_assoc="cramer",
+    fmt=".1f",
+    figsize=(8, 8),
+    plot=False,
+    cbar=True
+)
+plt.tight_layout()
+plt.show()
+
+# %%
+
+# plotting input features
+
+fig, axes = create_subplots(10, figsize=(14,10))
+
+for col, ax in zip(range(X.shape[1]), axes.flatten()):
+
+    pie(X_array[:, col], ax=ax,
+        ax_kws=dict(title=f'input feature {col+1}'), show=False)
+
+plt.tight_layout()
+plt.show()
+
+# %%
+
+# pie chart of target
+
+pie(y, ax_kws=dict(title='Target'))git add
