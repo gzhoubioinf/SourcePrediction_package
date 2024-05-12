0abc20a4ac09d0d1e6aa04a9064531ea4cc151be
Sara-Iftikhar <sara.rwpk@gmail.com>
2024-05-12 16:48:58 +0300

    Update stacked_ens.py

--- scripts/stacked_ens.py
+++ scripts/stacked_ens.py
@@ -4,29 +4,22 @@
 ==========================
 """
 
-
-import os
-
 import numpy as np
-import pandas as pd
-import xarray as xr
-
-from xgboost import XGBClassifier
 import matplotlib.pyplot as plt
-
+from sklearn import metrics as sk_metrics
+from sklearn.metrics import roc_auc_score
+from yellowbrick.classifier import ROCAUC
 from sklearn.linear_model import LogisticRegression
-
 from yellowbrick.classifier import ClassPredictionError
 from yellowbrick.classifier import PrecisionRecallCurve
-from yellowbrick.classifier import ROCAUC
 
 from ai4water import Model
+from easy_mpl import bar_chart
+from SeqMetrics import ClassificationMetrics
 from ai4water.utils.utils import TrainTestSplit
 from ai4water.postprocessing import ProcessPredictions
 
-from SeqMetrics import ClassificationMetrics
-
-from utils import load_data_from_pickle, plot_confidence_interval, get_data
+from utils import read_data
 
 USE_TRAINED_MODEL = False
 
@@ -42,7 +35,7 @@ def f1_score_macro(t,p)->float:
 
 target = 'source_prediction'
 
-X, y, input_features = get_data('data_5_test.nc', 'inputs_5.csv')
+X, y, input_features = read_data('data_5_test.nc', 'inputs_5.csv')
 
 # %%
 
@@ -64,9 +57,26 @@ print('ValY shape', ValY.shape)
 
 # %%
 
+# metrics lists
+
+# F1
+f1_score = []
+# Precision
+precision = []
+# Accuracy
+accuracy = []
+# recall
+recall = []
+# AUC
+auc = []
+# ROC
+roc_auc_score_list = []
+
+# %%
+
 basemodel_1 = 'XGBClassifier'
 basemodel_2 = 'RandomForestClassifier'
-basemodel_3 = 'DecisionTreeClassifier'
+basemodel_3 = 'LogisticRegression'
 meta_model_ = 'LogisticRegression'
 
 # %%
@@ -86,7 +96,16 @@ metrics = ClassificationMetrics(ValY.values, bm_1_pred)
 print(f'Base Model 1 -> Accuracy: {metrics.accuracy()}')
 print(f'Base Model 1 -> Precision: {metrics.precision()}')
 print(f'Base Model 1 -> Recall: {metrics.recall()}')
-print(f'Base Model 1 -> F1 score: {f1_score_macro(ValY.values, bm_1_pred)}')
+print(f'Base Model 1 -> F1 score: {metrics.f1_score(average="macro")}')
+accuracy.append(metrics.accuracy())
+precision.append(metrics.precision(average='macro'))
+recall.append(metrics.recall(average='macro'))
+f1_score.append(metrics.f1_score(average='macro'))
+
+fpr, tpr, _ = sk_metrics.roc_curve(ValY.values, bm_1_pred, pos_label=2)
+auc.append(sk_metrics.auc(fpr, tpr))
+
+roc_auc_score_list.append(roc_auc_score(ValY.values, base_model_1._model.predict_proba(ValX), multi_class='ovr'))
 
 # %%
 
@@ -107,6 +126,16 @@ print(f'Base Model 2 -> Precision: {metrics.precision()}')
 print(f'Base Model 2 -> Recall: {metrics.recall()}')
 print(f'Base Model 2 -> F1 score: {f1_score_macro(ValY.values, bm_2_pred)}')
 
+accuracy.append(metrics.accuracy())
+precision.append(metrics.precision(average='macro'))
+recall.append(metrics.recall(average='macro'))
+f1_score.append(metrics.f1_score(average='macro'))
+
+fpr, tpr, _ = sk_metrics.roc_curve(ValY.values, bm_2_pred, pos_label=2)
+auc.append(sk_metrics.auc(fpr, tpr))
+
+roc_auc_score_list.append(roc_auc_score(ValY.values, base_model_2._model.predict_proba(ValX), multi_class='ovr'))
+
 # %%
 
 base_model_3 = Model(
@@ -125,6 +154,17 @@ print(f'Base Model 3 -> Accuracy: {metrics.accuracy()}')
 print(f'Base Model 3 -> Precision: {metrics.precision()}')
 print(f'Base Model 3 -> Recall: {metrics.recall()}')
 print(f'Base Model 3 -> F1 score: {f1_score_macro(ValY.values, bm_3_pred)}')
+
+accuracy.append(metrics.accuracy())
+precision.append(metrics.precision(average='macro'))
+recall.append(metrics.recall(average='macro'))
+f1_score.append(metrics.f1_score(average='macro'))
+
+fpr, tpr, _ = sk_metrics.roc_curve(ValY.values, bm_3_pred, pos_label=2)
+auc.append(sk_metrics.auc(fpr, tpr))
+
+roc_auc_score_list.append(roc_auc_score(ValY.values, base_model_3._model.predict_proba(ValX), multi_class='ovr'))
+
 
 # %%
 
@@ -163,6 +203,16 @@ print(f'Meta Model -> Accuracy: {metrics.accuracy()}')
 print(f'Meta Model -> Precision: {metrics.precision()}')
 print(f'Meta Model -> Recall: {metrics.recall()}')
 print(f'Meta Model -> F1 score: {f1_score_macro(Y_new.values, meta_pred)}')
+
+accuracy.append(metrics.accuracy())
+precision.append(metrics.precision(average='macro'))
+recall.append(metrics.recall(average='macro'))
+f1_score.append(metrics.f1_score(average='macro'))
+
+fpr, tpr, _ = sk_metrics.roc_curve(Y_new.values, meta_pred, pos_label=2)
+auc.append(sk_metrics.auc(fpr, tpr))
+
+roc_auc_score_list.append(roc_auc_score(Y_new.values, meta_model._model.predict_proba(X_new_meta), multi_class='ovr'))
 
 # %%
 
@@ -220,3 +270,108 @@ viz = PrecisionRecallCurve(LogisticRegression(), classes=classes)
 viz.fit(X_val_meta, ValY.iloc[:,0])        # Fit the training data to the visualizer
 viz.score(X_new_meta, Y_new.values.reshape(-1,))        # Evaluate the model on the test data
 viz.show("figures/PrecisionRecallCurve_stacked_meta_test.png", dpi=600, bbox_inches="tight")
+
+# %%
+
+names = ['XGB', 'RF', 'LR', 'Stacked']
+
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(f1_score, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('F1_score')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
+#
+# %%
+
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(accuracy, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('Accuracy')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
+#
+# # %%
+#
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(precision, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('precision')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
+#
+# # %%
+#
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(recall, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('recall')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
+
+# %%
+
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(auc, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('AUC')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
+
+# %%
+
+names = ['XGB', 'RF', 'LR', 'Stacked']
+f, ax = plt.subplots(facecolor="#EFE9E6")
+
+bar_chart(roc_auc_score_list, cmap='summer_r',
+          orient='v', labels=names, ax=ax,
+          show=False)
+
+ax.grid(axis='y', ls='dotted', color='lightgrey')
+ax.set_title('ROC AUC Score')
+
+for spine in ax.spines.values():
+    spine.set_edgecolor('lightgrey')
+    spine.set_linestyle('dashed')
+
+plt.show()
